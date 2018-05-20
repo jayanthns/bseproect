ce0c966a794f9785cb8eeafa13fbcd3bcfe4e679
nsjayanth <jayanth.ns@avetoconsulting.com>
2018-05-20 16:09:33 +0530

    server ip added

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     }
     webapp = Index()
     webapp.get_users = UserService()
-    
+
     cherrypy.config.update({
         'server.socket_host': '13.127.129.1',
         'server.socket_port': 80,
